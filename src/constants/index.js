06ffbe92ae4413f6755433853c5837ea8b5c6992
duongmaiGH <duongmai1258@gmail.com>
2020-09-15 08:22:04 +0700

    commit on master

---
+++ src/constants/index.js
@@ -0,0 +1,17 @@
+import Login from "../container/Login";
+import dashboard from "../container/dashboard";
+
+export const ADMIN_ROUTES = [
+    {
+      path : '/',
+      name : 'Đăng Nhập',
+      exact : true,
+      component : Login,
+    },
+    {
+      path : '/Dashboard',
+      name : 'Dashboard',
+      exact : true,
+      component : dashboard,
+    }
+  ];
