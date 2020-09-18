931b96422e12fb2f122c389115df37b45c4bcab6
duongmaiGH <duongmai1258@gmail.com>
2020-09-18 16:50:28 +0700

    duan

--- src/container/dangnhap/index.js
+++ src/container/dangnhap/index.js
@@ -1,63 +1,104 @@
-import React, { Component } from "react";
 import {
   Button,
   Grid,
+  InputAdornment,
   TextField,
   Typography,
   withStyles,
 } from "@material-ui/core";
-import styles from "./styles";
-
 import Card from "@material-ui/core/Card";
 import CardContent from "@material-ui/core/CardContent";
+import { AccountCircle, Lock } from "@material-ui/icons";
+import clsx from "clsx";
+import React, { Component } from "react";
+import styles from "./styles";
 
 class dangnhap extends Component {
   render() {
     const { classes } = this.props;
+    
     return (
       <div className={classes.background}>
         <div className={classes.login}>
+          {/* <Grid item xs={12}>
+            <img
+              src="https://i.imgur.com/zkO97SN.png"
+              alt="logo"
+              className={classes.image}
+            />
+          </Grid> */}
+          <div className={classes.cardavt}>
+            <img
+              src="https://i.imgur.com/zkO97SN.png"
+              alt="logo"
+              
+              className={classes.image}
+            />
+          </div>
           <Card className={classes.card}>
             <div className={classes.root}>
-              {/* <Grid container spacing={2}>
-                  <Grid item xs={6}>
-                  <img src="https://i.imgur.com/QTYyn0d.png" alt="logo" className={classes.image}/>
-                  </Grid>
-                  <Grid item xs={6}>
-                    
-                  </Grid>
-                </Grid> */}
-                <CardContent>
-                      <form className={classes.form}>
-                        <div className={classes.typo}>
-                          <Typography variant="inherit">
-                            Đăng Nhập Để Tiếp Tục ?
-                          </Typography>
-                        </div>
+              <Grid container spacing={2}>
+                <Grid item xs={12}>
+                  <CardContent>
+                    <form className={classes.form} autoComplete="off">
+                      <div className={clsx(classes.editInput)}>
                         <label className={classes.set}>Tài Khoản</label>
                         <TextField
-                          id="email"
-                          labal="email"
-                          className={classes.TextField}
+                          
+                          id="username"
+                          type="email"
+                          
+                          required
                           fullWidth
-                          margin="normal"
-                          placeholder="Nhập Tài Khoản"
+                          placeholder="Nhập Tài Khoản..."
+                          InputProps={{
+                            className: classes.user,
+                            startAdornment: (
+                              <InputAdornment position="start">
+                                <AccountCircle />
+                              </InputAdornment>
+                            ),
+                          }}
                         />
                         <label className={classes.set}>Mật Khẩu</label>
                         <TextField
+                          classes={{
+                            text: classes.inputRoot,
+                            input: classes.inputInput,
+                          }}
                           id="password"
-                          labal="password"
-                          className={classes.TextField}
-                          type="password"
+                          required
                           fullWidth
-                          margin="normal"
-                          placeholder="Nhập Mật Khẩu"
+                          type="password"
+                          placeholder="Nhập Mật Khẩu..."
+                          InputProps={{
+                            className : classes.pass,
+                            startAdornment: (
+                              <InputAdornment position="start">
+                                <Lock />
+                              </InputAdornment>
+                            ),
+                          }}
                         />
-                        <Button variant="contained" color="primary" fullWidth>
-                          Login
+                        <Button
+                          type="submit"
+                          fullWidth
+                          variant="contained"
+                          color="primary"
+                          className={classes.button}
+                        >
+                          Đăng Nhập
                         </Button>
-                      </form>
-                    </CardContent>
+                        <a href="/#" className={classes.a}>
+                          <Typography className={classes.Fpass}>
+                            Quên Mật Khẩu
+                          </Typography>
+                        </a>
+                      </div>
+                    </form>
+                  </CardContent>
+                </Grid>
+              </Grid>
             </div>
           </Card>
         </div>
--- src/container/dangnhap/styles.js
+++ src/container/dangnhap/styles.js
@@ -1,6 +1,6 @@
 const styles = (theme) => ({
   background: {
-    backgroundColor: theme.palette.primary.main,
+    // backgroundColor: theme.palette.primary.main,
     padding: 40,
     display: "flex",
     flexDirection: "column",
@@ -9,26 +9,68 @@ const styles = (theme) => ({
     minHeight: "100vh",
     flex: " 1 0 auto",
     backgroundSize : '100%',
-    backgroundImage : `URL(${'https://images.pexels.com/photos/772803/pexels-photo-772803.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'})`,
+    backgroundImage : `URL(${'https://i.imgur.com/6T8GlYL.jpg'})`,
+  },
+  editInput : {
+    color: '#f3f3f3 !important',
+    
   },
   card : {
     borderRadius : (10,10,10,10),
     boxShadow: '0 4px 8px 0 rgba(0, 0, 0, 0.3), 0 6px 20px 0 rgba(0, 0, 0, 0.19)',
     background  : 'none',
+    maxWidth : '400px',
+  },
+  cardavt : {
+    
+    borderRadius : (10,10,10,10),
+    boxShadow: '0 4px 8px 0 rgba(0, 0, 0, 0.3), 0 6px 20px 0 rgba(0, 0, 0, 0.19)',
+    background  : 'white',
+    maxWidth : '120px',
+    maxHeight: '90px',
+    margin: 'Auto',
   },
   typo: {
     textAlign: "center",
     fontSize: "20px",
+    color : 'white',
   },
   root: {
     flexGrow: 1,
   },
   image: {
-    marginTop : '10px',
+    marginTop : '-50px',
     display : 'block',
     marginLeft : 'auto',
     marginRight : 'auto',
-    width: "250px",
+    height: '70px',
+    width: "70px",
   },
+  set : {
+    color : 'white',
+  },
+  Fpass : {
+    paddingTop : '20px',
+    textAlign: "center",
+    fontSize: "13px",
+    color : 'grey',
+  },
+  a : {
+    textDecoration : 'none',
+  },
+  button : {
+    marginTop : '10px',
+  },
+  user : {
+    marginTop : '10px',
+    color: 'white',
+    marginBottom : '15px'
+  },
+  pass : {
+    marginTop : '10px',
+    color: 'white',
+    marginBottom : '20px'
+  },
+  
 });
 export default styles;
